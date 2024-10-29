64243383abf644c0e0d5ff901b1ab0bb6cd3ad53
AmelSAT <aselatt079@g.educaand.es>
2024-10-29 22:11:26 +0100

    añadimos nuestro proyecto de sumas

---
+++ sumaentorno/src/sumaentorno/sumaDosNumeros.java
@@ -0,0 +1,12 @@
+package sumaentorno;
+
+public class sumaDosNumeros {
+
+	public static void main(String[] args) {
+		        int a = 5;
+		        int b = 10;
+		        int suma = a + b;
+		        System.out.println("La suma es: " + suma);
+		    }
+		}
+
